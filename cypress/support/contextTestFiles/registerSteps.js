19087c2179692ee31f1a65de664b200b40d601c3
ldino95 <31542168+ldino95@users.noreply.github.com>
2021-07-01 15:38:10 -0300

    commit

---
+++ cypress/support/contextTestFiles/registerSteps.js
@@ -0,0 +1,18 @@
+/* global Given, Then, When */ 
+/// <reference types="Cypress"/> 
+
+Given('que estou na pagina de registro', () => {
+	cy.visit('/');
+});
+
+When('preencho campos corretamente', () => {
+
+});
+
+Then('clico no botao de finalizar cadastro', () => {
+	
+});
+
+Then('mensagem de sucesso e exibida', () => {
+
+});
